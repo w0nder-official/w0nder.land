d309617dfbb3091e90806557617e9f28ba9eeb79
jh.park <jh.park@fi-workers.com>
2024-08-23 16:48:18 +0900

    :wrench: Naver site varify 추가

--- src/w0nder-land/src/pages/_document.tsx
+++ src/w0nder-land/src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="naver-site-verification" content="963e16f6001702e8c4a4c23f53d37fbbf677fe65" />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
