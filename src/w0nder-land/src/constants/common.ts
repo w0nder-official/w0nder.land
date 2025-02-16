cacf692fdd145d7fdd2136f3fdddc67cc8c3386d
w0nder <154417771+w0nder-official@users.noreply.github.com>
2025-02-16 23:14:45 +0900

    Update common.ts

--- src/w0nder-land/src/constants/common.ts
+++ src/w0nder-land/src/constants/common.ts
@@ -3,7 +3,7 @@ export const TwitterLink = 'https://twitter.com/w0nder_official';
 export const ShowYourTimeLink = 'https://showyourti.me/qr';
 export const FiWorkersLink = 'https://fi-workers.com';
 export const GithubLink = 'https://github.com/w0nder-official';
-export const CoffeeChatLink = 'https://cal.com/w0nder/coffee-chat';
+export const CoffeeChatLink = 'https://tally.so/r/3jGqZa';
 
 export enum Environment {
   Local = 'local',
