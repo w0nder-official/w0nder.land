b29c4f28473db9babebda663c1114d875f272f8b
w0nder <alice@w0nder.land>
2025-05-29 17:10:34 +0900

    커피챗 링크 변경

--- src/w0nder-land/src/constants/common.ts
+++ src/w0nder-land/src/constants/common.ts
@@ -3,7 +3,7 @@ export const TwitterLink = 'https://twitter.com/w0nder_official';
 export const ShowYourTimeLink = 'https://showyourti.me/qr';
 export const FiWorkersLink = 'https://fi-workers.com';
 export const GithubLink = 'https://github.com/w0nder-official';
-export const CoffeeChatLink = 'https://tally.so/r/3jGqZa';
+export const CoffeeChatLink = 'https://www.latpeed.com/products/rSNlW';
 
 export enum Environment {
   Local = 'local',
