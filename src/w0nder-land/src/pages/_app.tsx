8ac0053b50e01ec24ac0b0739ec5bfe291e263e9
w0nder <alice@w0nder.land>
2025-07-28 02:21:58 +0900

    배경색 변경 및 border 정리

--- src/w0nder-land/src/pages/_app.tsx
+++ src/w0nder-land/src/pages/_app.tsx
@@ -8,7 +8,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       </Head>
-      <Component {...pageProps} />
+      <div className="border-t border-b border-gray-200">
+        <Component {...pageProps} />
+      </div>
     </>
   );
 }
