315330481041591a07c071e3afcd3cded9a3fe2d
w0nder <alice@w0nder.land>
2025-12-11 18:30:11 +0900

    env 로드 안되서 그냥 하드코딩함

--- src/w0nder-land/src/constants/configure.ts
+++ src/w0nder-land/src/constants/configure.ts
@@ -2,11 +2,11 @@ import * as process from 'process';
 import { Environment } from '@/constants/common';
 
 export class Configure {
-  static readonly Environment = process.env.ENVIRONMENT || Environment.Local;
+  static readonly Environment = process.env.ENVIRONMENT || Environment.Production;
 
-  static readonly ServiceUrl = process.env.NEXT_PUBLIC_SERVICE_URL || 'http://localhost:3000';
+  static readonly ServiceUrl = process.env.NEXT_PUBLIC_SERVICE_URL || 'https://w0nder.land';
 
-  static readonly GaTrackingId = process.env.NEXT_PUBLIC_GA_TRACKING_ID || '';
+  static readonly GaTrackingId = process.env.NEXT_PUBLIC_GA_TRACKING_ID || 'G-X5RKHMNZ1P';
 
   static readonly SlackAccessToken = process.env.SLACK_ACCESS_TOKEN || '';
 
